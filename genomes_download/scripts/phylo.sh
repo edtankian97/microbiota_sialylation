42606c237afcd83628ff03b476154458a146693d
edtankian97 <edson_alexandre97@usp.br>
2026-01-12 17:21:48 -0300

    add new scripts

--- genomes_download/scripts/phylo.sh
+++ genomes_download/scripts/phylo.sh
@@ -1,4 +1,4 @@
 #! bin/bash
 
 
-phylophlan -i ../proteins/proteins_comm_sia/proteins_unique_comm_sia/ --db_type a -d phylophlan --databases_folder ../proteins/protein_tree/phylophlan_database/ --diversity low --accurate -f ../proteins/protein_tree/genome_ed_config.cfg -o ../proteins/protein_tree/phylo_result/  --nproc 15  --verbose 2>&1 | tee ../proteins/protein_tree/logs/phylophlan_output_phylophlan.log
+phylophlan -i ../proteins/proteins_sialylation/final_complete_sialylation/ --db_type a -d phylophlan --databases_folder ../proteins/protein_tree/phylophlan_database/ --diversity medium --accurate -f ../proteins/protein_tree/genome_ed_config.cfg -o ../proteins/protein_tree/phylo_result/  --nproc 15  --verbose 2>&1 | tee ../proteins/protein_tree/logs/phylophlan_output_phylophlan.log
